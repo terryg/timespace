859749abce9a57e414b1077413dd7e1c2af5ce89
Terry Lorber <tgl@rideside.net>
2014-04-15 15:43:09 +0000

    Removing debugging command.

--- app.rb
+++ app.rb
@@ -3,8 +3,12 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
   get '/' do
+    @initial = Date.new(1726, 5, 15)
+    @next = Date.new(1899, 10, 01)
+    @now = Date.today()
+    @final = @next + (@next - @initial)
+    @days_remaining = (@final - @now).to_i 
     haml :index
   end
 
-  set :bind, '0.0.0.0'
 end
--- config.ru
+++ config.ru
@@ -1,3 +1,3 @@
-require './'
+require './app'
 
 run App
